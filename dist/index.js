b2a2cbdb5a86b34451b9a86f2df3c93cbad05921
somenerd_bootdev <somenerdbootdev@gmail.com>
2026-04-27 16:10:17 -0400

    Who cares, really

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,21 @@
 import express from "express";
 const app = express();
 const PORT = 8080;
+const middlewareLogResponses = (req, res, next) => {
+    res.on("finish", () => {
+        if (res.statusCode != 200) {
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`);
+        }
+    });
+    next();
+};
+app.use(middlewareLogResponses);
 app.use("/app", express.static("./src/app"));
 const handlerReadiness = (req, res) => {
     res.set("Content-Type", "text/plain; charset=utf-8");
-    res.send("OK"); //req.body)
+    res.send("OK");
 };
-app.get("/healthz", handlerReadiness);
+app.get("/healthz", middlewareLogResponses, handlerReadiness);
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
 import express from "express";
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 
 const app = express();
 const PORT = 8080;
 
+const middlewareLogResponses = (req: Request, res: Response, next: NextFunction) => {
+    res.on("finish", () => {
+        if (res.statusCode != 200) {
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`)
+        }
+    });
+    next();
+}
+app.use(middlewareLogResponses)
 app.use("/app", express.static("./src/app"));
 
 const handlerReadiness = (req: Request, res: Response) => {
-    res.set("Content-Type", "text/plain; charset=utf-8")
-    res.send("OK")
+    res.set("Content-Type", "text/plain; charset=utf-8");
+    res.send("OK");
 }
-app.get("/healthz", handlerReadiness);
+app.get("/healthz", middlewareLogResponses, handlerReadiness);
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
